ca8e6a51262cac700b49c47e08dac3b198d6b342
HBchevelle68 <aportante10@gmail.com>
2017-02-10 14:37:32 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,22 +3,14 @@
 #include <thread>
 
 #include "Benchmark.h"
+
 using namespace std;
 
 void test(){for (int i = 0; i<99994523; ++i){}}
 
-
-
-
 int main() {
-
-	std::cout << "norm: " << ftime_ms(test) << endl;
-	
-	std::cout << "norm: " << ftime_fms(test) << endl;
-	
-	std::cout << "norm: " << ftime_dms(test) << endl;
-
-
-
+	std::cout << "seconds (int): " << ftime_sec(test) << endl;
+	std::cout << "seconds (float): " << ftime_fsec(test) << endl;	
+	std::cout << "seconds (double): " << ftime_dsec(test) << endl;
 	return 0;
 }
